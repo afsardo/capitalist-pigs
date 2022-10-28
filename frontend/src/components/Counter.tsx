e763720534993ee9c3964dc14da2b85639964faa
afsardo <afsardo@gmail.com>
2022-10-28 18:15:38 +0100

    fix: issue with fuels package on ssr

---
+++ frontend/src/components/Counter.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { Wallet } from "fuels";
+import { ContractsAbi__factory } from "../contracts";
+
+// The private key of the `owner` in chainConfig.json.
+// This enables us to have an account with an initial balance.
+const WALLET_SECRET =
+  "0xa449b1ffee0e2205fa924c6740cc48b3b473aa28587df6dab12abc245d1f5298";
+
+// The ID of the contract deployed to our local node.
+// The contract ID is displayed when the `forc deploy` command is run.
+const CONTRACT_ID =
+  "0xd97ac31a1a473e6dd6f7dfdaf773a2a4df452ec3445afd291298b48dcea57cd3";
+
+// Create a "Wallet" using the private key above.
+const wallet = new Wallet(WALLET_SECRET);
+
+const contract = ContractsAbi__factory.connect(CONTRACT_ID, wallet);
+
+export default function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    async function main() {
+      // Executes the `counter()` function to query the current contract state.
+      // the `.get()` method is read-only. Therefore, doesn't spend coins.
+      const { value } = await contract.functions.counter().get();
+      setCounter(Number(value));
+    }
+    main();
+  }, []);
+
+  async function increment() {
+    // Creates a transactions to call the `increment()` function, passing in
+    // the amount we want to increment. Because we're creating a TX that updates
+    // the contract state, this requires the wallet to have enough coins to
+    // cover the costs and to sign the transaction.
+    const { value } = await contract.functions.increment(1).call();
+    setCounter(Number(value));
+  }
+
+  return (
+    <>
+      <p>Counter: {counter}</p>
+      <button onClick={increment}>Increment</button>
+    </>
+  );
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,44 +1,10 @@
-import { Wallet } from "fuels";
-import { useEffect, useState } from "react";
-import { ContractsAbi__factory } from "../contracts";
+import dynamic from "next/dynamic";
 
-// The private key of the `owner` in chainConfig.json.
-// This enables us to have an account with an initial balance.
-const WALLET_SECRET =
-  "0xa449b1ffee0e2205fa924c6740cc48b3b473aa28587df6dab12abc245d1f5298";
-
-// The ID of the contract deployed to our local node.
-// The contract ID is displayed when the `forc deploy` command is run.
-const CONTRACT_ID =
-  "0xd97ac31a1a473e6dd6f7dfdaf773a2a4df452ec3445afd291298b48dcea57cd3";
-
-// Create a "Wallet" using the private key above.
-const wallet = new Wallet(WALLET_SECRET);
-
-const contract = ContractsAbi__factory.connect(CONTRACT_ID, wallet);
+const Counter = dynamic(() => import("../components/Counter"), {
+  ssr: false,
+});
 
 export default function Home() {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    async function main() {
-      // Executes the `counter()` function to query the current contract state.
-      // the `.get()` method is read-only. Therefore, doesn't spend coins.
-      const { value } = await contract.functions.counter().get();
-      setCounter(Number(value));
-    }
-    main();
-  }, []);
-
-  async function increment() {
-    // Creates a transactions to call the `increment()` function, passing in
-    // the amount we want to increment. Because we're creating a TX that updates
-    // the contract state, this requires the wallet to have enough coins to
-    // cover the costs and to sign the transaction.
-    const { value } = await contract.functions.increment(1).call();
-    setCounter(Number(value));
-  }
-
   return (
     <div>
       <header>
@@ -46,8 +12,7 @@ export default function Home() {
       </header>
 
       <main>
-        <p>Counter: {counter}</p>
-        <button onClick={increment}>Increment</button>
+        <Counter />
       </main>
 
       <footer>Powered b Degens, BlazinglyFast™</footer>
