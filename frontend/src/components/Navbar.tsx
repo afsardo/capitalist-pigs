bae7bce61d9fdba23ecaaf0992c27bd41939fe01
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-10-28 19:08:59 +0100

    style: navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,29 +1,31 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+const routes = [
+  { href: "/", label: "Home" },
+  { href: "/dashboard", label: "Dashboard" },
+  { href: "/lab", label: "Laboratory" },
+  { href: "/coop", label: "Co-op" },
+];
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
-    <ul className="flex justify-center gap-5 py-4 px-4">
-      <li>
-        <Link className="hover:text-yellow-200" href="/">
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link className="hover:text-yellow-200" href="dashboard">
-          Dashboard
-        </Link>
-      </li>
-      <li>
-        <Link className="hover:text-yellow-200" href="lab">
-          Laboratory
-        </Link>
-      </li>
-      <li>
-        <Link className="hover:text-yellow-200" href="coop">
-          Co-op
-        </Link>
-      </li>
+    <ul className="flex justify-center gap-5 py-4 px-4 text-sm">
+      {routes.map((route) => (
+        <li key={route.href}>
+          <Link
+            className={`${
+              router.pathname === route.href ? "" : "text-gray-500"
+            } hover:text-white`}
+            href={route.href}
+          >
+            {route.label}
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 };
