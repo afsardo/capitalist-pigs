c05d09774433bec87952eaf24af4b2c605e76257
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-10-29 21:31:29 +0100

    modal z index

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -11,7 +11,7 @@ type Props = {
 const Modal = ({ isOpen, onClose, children, title }: Props) => {
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={onClose}>
+      <Dialog as="div" className="relative z-40" onClose={onClose}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
