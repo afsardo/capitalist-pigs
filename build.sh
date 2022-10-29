96281c7b1a3a72bffd6e10bac4960b43ba4e8a8f
afsardo <afsardo@gmail.com>
2022-10-29 19:37:49 +0100

    fix: issue with frontend build contracts

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 forc build -p contracts/src/bacon
-forc build -p contracts/src/bacon --output-directory contracts/out
+forc build -p contracts/src/bacon --output-directory contracts/out/debug
 forc build -p contracts/src/bacon_distributor
-forc build -p contracts/src/bacon_distributor --output-directory contracts/out
+forc build -p contracts/src/bacon_distributor --output-directory contracts/out/debug
 forc build -p contracts/src/pigs
-forc build -p contracts/src/pigs --output-directory contracts/out
+forc build -p contracts/src/pigs --output-directory contracts/out/debug
 forc build -p contracts/src/piglets
-forc build -p contracts/src/piglets --output-directory contracts/out
+forc build -p contracts/src/piglets --output-directory contracts/out/debug
 forc build -p contracts/src/staking
-forc build -p contracts/src/staking --output-directory contracts/out
+forc build -p contracts/src/staking --output-directory contracts/out/debug
 forc build -p contracts/src/truffles
-forc build -p contracts/src/truffles --output-directory contracts/out
+forc build -p contracts/src/truffles --output-directory contracts/out/debug
 forc build -p contracts/src/truffles_pool
-forc build -p contracts/src/truffles_pool --output-directory contracts/out
+forc build -p contracts/src/truffles_pool --output-directory contracts/out/debug
