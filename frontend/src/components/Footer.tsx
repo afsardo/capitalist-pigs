9c182f738e9e3a39bb32343c36ffd8f71ca4fa5b
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2022-10-29 15:47:40 +0100

    footer icons and asset count

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 const Footer = () => {
+  const assets = {
+    pigs: 0,
+    piglets: 0,
+    truffles: 0,
+  };
+
   return (
-    <footer className="fixed bottom-0 w-full h-[80px] justify-center items-center flex justify-between px-5 bg-gray-800">
-      <div>© 2022 Powered by Degens, BlazinglyFast</div>
+    <footer className="fixed bottom-0 w-full h-[40px] items-center flex justify-between px-5 bg-gray-800">
+      <div className="text-xs">© 2022 Powered by Degens, BlazinglyFast</div>
       <div className="flex space-x-5">
-        <div>PIG</div>
-        <div>PIGLET</div>
-        <div>TRUFFLES</div>
+        <div>{assets.pigs} 🐖</div>
+        <div>{assets.piglets} 🐷</div>
+        <div>{assets.truffles} 🌰</div>
       </div>
     </footer>
   );
