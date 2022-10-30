4d63bf7ae546fbf93e5d6d9e1b2009eda988c150
Dynalz <p.mauricio@campus.fct.unl.pt>
2022-10-30 01:31:06 +0000

    :hammer: footer bigger

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,8 +8,8 @@ const Footer = () => {
   const baconCount = useAllOutLifeStore((s) => s.baconCount);
 
   return (
-    <footer className="fixed bottom-0 w-full h-[60px] items-center flex justify-between px-5 bg-transparent backdrop-blur-sm border-t border-white/10 z-30">
-      <div className="text-xs">
+    <footer className="fixed bottom-0 w-full h-[120px] items-center flex justify-between px-5 bg-transparent backdrop-blur-sm border-t border-white/10 z-30 gg">
+      <div className="text-gg">
         © {new Date().getFullYear()} Powered by Degens, BlazinglyFast
       </div>
       <div className="flex space-x-5">
